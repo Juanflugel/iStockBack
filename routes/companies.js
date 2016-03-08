4fff197b00fa13e86935758f2f36e38ad249a51a
Juanflugel <juanjosegarcian1@gmail.com>
2016-03-08 20:45:06 +0100

    company route

--- routes/companies.js
+++ routes/companies.js
@@ -6,12 +6,30 @@ function companies (app,Company){
 	app.use(bodyParser.json());
 
 	app.get('/company',findCompany);
+	app.post('/company',newCompany);
+	app.put('/company',updateCompany);
 
 	function findCompany (req,res){
 		Company.find({},function (err,array){
 			res.json(array);
 		});
 	}
+
+	function newCompany(req,res){
+		Company.create(req.body,function (err,obj){
+			res.json(obj);
+		});
+	}
+
+	function updateCompany (req,res){
+		var query = req.query;
+
+		var todo = req.body;
+            Company.findOneAndUpdate({'_id':query.idCompany},todo,
+            {new:true},function (err,obj){
+                res.json(obj);
+            });
+        }   
 
 
 }
